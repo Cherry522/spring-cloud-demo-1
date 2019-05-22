ce54e06f5d2721065a18e09775a3b132d9b6da8b
chenyan <522581280@qq.com>
2019-05-22 14:32:43 +0800

    添加config配置文件存放目录

--- config-server/src/main/java/com/demo/cherry/config/ConfigApplication.java
+++ config-server/src/main/java/com/demo/cherry/config/ConfigApplication.java
@@ -2,7 +2,9 @@ package com.demo.cherry.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer//激活对配置中心的支持
 @SpringBootApplication
 public class ConfigApplication {
 
