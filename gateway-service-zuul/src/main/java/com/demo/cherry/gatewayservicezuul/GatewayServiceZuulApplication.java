a6eb9c760d05bcdee81bdfb1e06dfe2ba711e861
chenyan <522581280@qq.com>
2019-05-27 19:21:55 +0800

    添加zuul

---
+++ gateway-service-zuul/src/main/java/com/demo/cherry/gatewayservicezuul/GatewayServiceZuulApplication.java
@@ -0,0 +1,15 @@
+package com.demo.cherry.gatewayservicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy//启用网关路由
+public class GatewayServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceZuulApplication.class, args);
+    }
+
+}
